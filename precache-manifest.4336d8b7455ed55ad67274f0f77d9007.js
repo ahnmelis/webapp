8203116deab03e7a23b749d60afb21043df3856a
ahnmelis <ahnmelis@usc.edu>
2021-08-24 23:09:49 -0700

    Updates

---
+++ precache-manifest.4336d8b7455ed55ad67274f0f77d9007.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d70ff15a19646c0bdb9c315a67efa6",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "44f2a5bb4640e07b151f",
+    "url": "/webapp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "53ed3b87cc0e6021c8e1",
+    "url": "/webapp/static/css/main.117f7c58.chunk.css"
+  },
+  {
+    "revision": "44f2a5bb4640e07b151f",
+    "url": "/webapp/static/js/2.e00e3d35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/webapp/static/js/2.e00e3d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ed3b87cc0e6021c8e1",
+    "url": "/webapp/static/js/main.9cb7938a.chunk.js"
+  },
+  {
+    "revision": "65e6fc8a71b682e9063f",
+    "url": "/webapp/static/js/runtime-main.122b568d.js"
+  },
+  {
+    "revision": "2bee9aa2a8a5330a3925f6379ce0fd59",
+    "url": "/webapp/static/media/profile2020.2bee9aa2.jpg"
+  }
+]);
